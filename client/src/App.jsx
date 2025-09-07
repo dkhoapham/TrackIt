199e65e406acc50cfd3a678f8215d20f9dca8cdd
kevin_da_dino <pham.dangkhoa.se@gmail.com>
2025-09-07 11:42:33 -0700

    front-end setup

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
+
+const Root = () => {
+  //check if token is present in local storage
+  const isAuthenticated = localStorage.getItem("token");
+
+  //if token is present redirect to dashboard else redirect to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+}
